b8c4a855058aebe92b41f3d190d1dce196317d68
Chaya <chaya@gmail.com>
2026-08-13 23:24:05 +0530

    Remove Introduction section as requested in screenshot and compress 42 JPEG photos reducing payload size from 8.5MB down to 3.8MB

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
-import Introduction from "./components/Introduction";
 import Gallery from "./components/Gallery";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -16,7 +15,6 @@ export default function App() {
       <main className="relative z-10 max-w-full overflow-x-hidden">
         <Hero />
         <Services />
-        <Introduction />
         <Gallery />
         <About />
         <Contact />
